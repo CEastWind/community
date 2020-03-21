e52487dccf43be0019a47da5582b7bf52429e420
CEastWind <aliudonglin@qq.com>
2020-03-21 14:05:18 +0800

    Modify the way GitHub login gets access token

--- src/main/java/com/liudl/community/CommunityApplication.java
+++ src/main/java/com/liudl/community/CommunityApplication.java
@@ -3,6 +3,7 @@ package com.liudl.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 启动项目的类，springboot内置tomcat，不需要手动再放到tomcat中运行
